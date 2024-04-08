54c0a736296ebedab19ab23295002de2fc577b26
ogwen123 <owen.d.jones@btinternet.com>
2024-04-08 19:01:07 +0100

    allow disabling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import React from "react"
+import React from "react"
 import { Route, Routes } from 'react-router-dom'
 
 import Home from './components/routes/Home'
@@ -6,14 +6,51 @@ import PageHandler from './PageHandler'
 import Site from './components/routes/Site'
 
 const App = () => {
+
+  const [enabled, setEnabled] = React.useState<boolean>(true)
+
+  React.useEffect(() => {
+    let url
+    if (location.href.includes("localhost") || location.href.includes("127.0.0.1")) {
+      url = "http://localhost:3002/api/services/check"
+    } else {
+      url = "https://admin-api.owen-services.eu.org/api/services/check"
+    }
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        id: "c99fd840-bd7c-4a15-95f4-08a28285536a"
+      })
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          if (data.data.enabled !== undefined) {
+            setEnabled(data.data.enabled)
+          }
+        })
+      }
+    })
+  }, [])
+
   return (
     <div className=''>
-      <Routes>
-        <Route path="/" element={<PageHandler />}>
-          <Route index element={<Home />} />
-          <Route path="/site/:id" element={<Site />} />
-        </Route>
-      </Routes>
+      {
+        enabled ?
+          <Routes>
+            <Route path="/" element={<PageHandler />}>
+              <Route index element={<Home />} />
+              <Route path="/site/:id" element={<Site />} />
+            </Route>
+          </Routes>
+          :
+          <div className="text-xl h-[200px] w-full fc">
+            This site has been disabled by an administrator.
+          </div>
+      }
     </div>
   )
 }
